84b8fbdd46154bcba0b169749e750c488dfd6a47
Michael Esch <eschmichael@yandex.com>
2019-05-27 14:45:06 -0400

    Add some leader categories borrowed from spacemacs

--- init.el
+++ init.el
@@ -27,6 +27,12 @@
 ;; BEGIN default key bindings
 (use-package general :ensure t)
 (require 'general)
+;; The space leader that spacemacs made popular
+(general-create-definer leader-SPC
+ :states '(normal visual insert emacs)
+ :prefix "SPC"
+ :non-normal-prefix "C-SPC")
+
 (general-define-key
  "C-'" 'avy-goto-word-1
  "C-s" 'swiper             ; search for string in current buffer
@@ -39,30 +45,41 @@
 
  )
 
-(general-define-key
- :states '(normal visual insert emacs)
- :prefix "SPC"
- :non-normal-prefix "C-SPC"
+(leader-SPC
+ "a" '(:ignore t :which-key "applications")
+ "ar" 'ranger
+ "ad" 'dired
  "b" '(:ignore t :which-key "buffer")
  "bs" 'ivy-switch-buffer
+ "c" '(:ignore t :which-key "compile/comments")
+ "C" '(:ignore t :which-key "capture/colors")
+ "e" '(:ignore t :which-key "errors")
  "f" '(:ignore t :which-key "files")
  "ff" 'counsel-find-file  ; find file using ivy
  "fr" 'counsel-recentf    ; find recently edited files
+ "g" '(:ignore t :which-key "git/vc")
+ "h" '(:ignore t :which-key "help")
+ "i" '(:ignore t :which-key "insertion")
+ "j" '(:ignore t :which-key "join/split")
+ "m" '(:ignore t :which-key "major-mode-cmd")
+ "n" '(:ignore t :which-key "narrow")
  "p" '(:ignore t :which-key "project")
  "pf" 'counsel-git        ; find file in git project
  "pg" 'counsel-git-grep   ; find string in git project
+ "q" '(:ignore t :which-key "session/quit")
+ "r" '(:ignore t :which-key "registers")
+ "s" '(:ignore t :which-key "search")
+ "S" '(:ignore t :which-key "spelling")
+ "t" '(:ignore t :which-key "toggle/theme")
+ "w" '(:ignore t :which-key "windows")
+ "z" '(:ignore t :which-key "zoom")
 
  ;; simple command
  "'"   '(iterm-focus :which-key "iterm")
  "?"   '(iterm-goto-filedir-or-home :which-key "iterm - goto dir")
  "/"   'counsel-ag
- "TAB" '(switch-to-other-buffer :which-key "prev buffer")
- "SPC" '(avy-goto-word-or-subword-1  :which-key "go to char")
+ "SPC" '(avy-goto-word-or-subword-1  :which-key "avy word")
 
- ;; Applications
- "a" '(:ignore t :which-key "Applications")
- "ar" 'ranger
- "ad" 'dired)
 ;; END default key bindings
 (use-package counsel :ensure t)
 (use-package ivy :ensure t)
